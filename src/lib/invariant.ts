a7116e6f702e0ba08b23d108095213f3ae035be2
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-06-10 14:09:39 +0200

    chore: fix unused deps

--- src/lib/invariant.ts
+++
@@ -1,9 +0,0 @@
-export function invariant(
-  condition: unknown,
-  message?: string,
-): asserts condition {
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  if (!condition) {
-    throw new Error(message);
-  }
-}
