3481b78870521a3fd1a012b3580384d5d53ccba2
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-09-02 21:43:51 +0200

    chore: remove unused file

--- src/components/no-ssr.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from "next/dynamic";
-import type React from "react";
-
-export const withNoSSR = <T,>(Component: React.FunctionComponent<T>) =>
-  dynamic(() => Promise.resolve(Component), { ssr: false });
