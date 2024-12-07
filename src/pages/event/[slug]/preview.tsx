e27929da7bce4ff88e8b7a2d412fc91473c403c9
Przem B <przemb@protonmail.com>
2024-12-07 06:42:13 +0100

    fix: delete button working on single leaf blocks. cant delete a tree of blocks (prob db issue)

--- src/pages/event/[slug]/preview.tsx
+++ src/pages/event/[slug]/preview.tsx
@@ -178,6 +178,58 @@ const BlockDialog = ({
   );
 };
 
+const DeleteDialog = ({
+  children,
+  blockId,
+  onDelete
+}: {
+  children: ReactNode;
+  onDelete?: () => Promise<void> | void;
+  blockId: string;
+}) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+  const deleteBlock = useMutation({
+    mutationFn: async () => {
+      const block = supabase.from("blocks");
+
+      return block.delete().eq("blockId", blockId)
+    },
+  });
+
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild={true}>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>
+            Usuń
+          </DialogTitle>
+        </DialogHeader>
+        <p>Czy na pewno chcesz usunąć sekcje?</p>
+        <DialogFooter>
+          <Button onClick=
+            {async () => 
+              await deleteBlock
+                .mutateAsync()
+                .then(() => {
+                  onDelete?.()
+                  setIsOpen(false);
+                  toast("Usunięto sekcję");
+                }).catch(() => {
+                  setIsOpen(false);
+                  toast("Nie udało się usunąć sekcji");
+                })
+            }
+          >
+            Usuń sekcje
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+		</Dialog>
+	)
+};
+
 type Block = Tables<"blocks">;
 
 function buildBreadcrumbs(blocks: Block[], currentBlockId: string) {
@@ -320,7 +372,15 @@ const Preview = ({
                 <Button variant="ghost">Edytuj</Button>
               </BlockDialog>
 
-              <Button variant="ghost">Usuń</Button>
+              <DeleteDialog
+                blockId={blockId}
+                onDelete={async () => {
+                  await allBlocksQuery.refetch(); //na odwrót setBlockId i refetch, aby uniknąć mignięcia usuwanego blocku
+                  setBlockId(parentId ? parentId : null)
+                }}
+              >
+                <Button variant="ghost">Usuń</Button>
+              </DeleteDialog>
             </div>
           </>
         ) : null}
